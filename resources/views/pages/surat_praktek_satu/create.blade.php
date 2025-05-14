fc485e7e1e22aefd8b7642c487c241bfcecee7b5
Sigit Putra Prabowo <11230220@nusamandiri.ac.id>
2025-05-14 18:36:24 +0700

    Upload ulang project Laravel dengan .gitignore

--- resources/views/pages/surat_praktek_satu/create.blade.php
+++ resources/views/pages/surat_praktek_satu/create.blade.php
@@ -7,7 +7,7 @@
         <div class="row">
             <div class="col-12">
                 <div class="page-title-box d-flex justify-content-between align-items-md-center flex-md-row flex-column">
-                    <h4 class="page-title">Tambah Surat Keterangan Praktek</h4>
+                    <h4 class="page-title">Tambah Surat Keterangan Praktek rsud</h4>
                     <ol class="m-0 breadcrumb">
                         <li class="breadcrumb-item"><a href="#">Surat Praktek</a></li>
                         <li class="breadcrumb-item active">Tambah</li>
