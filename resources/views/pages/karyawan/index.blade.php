4a5e8bd60002d2c80ada87c98c4e184baba28f15
Sigit Putra Prabowo <11230220@nusamandiri.ac.id>
2025-06-25 16:05:43 +0700

    update

--- resources/views/pages/karyawan/index.blade.php
+++ resources/views/pages/karyawan/index.blade.php
@@ -147,7 +147,7 @@
                 initComplete: function() {
                     this.api().columns().every(function() {
                         var column = this;
-                        if (column.index() === 8) return; // Skip "Aksi" column
+                        if (column.index() === 9) return; // Skip "Aksi" column
 
                         var input = document.createElement("input");
                         input.style.width = '100%';
