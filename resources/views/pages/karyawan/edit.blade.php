28c100767acfd4e9f5467b3346ed689f586d182d
Sigit Putra Prabowo <11230220@nusamandiri.ac.id>
2025-06-26 13:50:46 +0700

    aa

--- resources/views/pages/karyawan/edit.blade.php
+++ resources/views/pages/karyawan/edit.blade.php
@@ -24,14 +24,14 @@
             <div class="alert alert-danger">{{ session('error') }}</div>
         @endif
 
-        <div class="card">
-            <div class="card-body">
-                <form action="{{ route('karyawan.update', $karyawan->id) }}" method="POST">
-                    @csrf
-                    @method('PUT')
+        <form action="{{ route('karyawan.update', $karyawan->id) }}" method="POST">
+            @csrf
+            @method('PUT')
 
-                    {{-- Data Pribadi --}}
-                    <h5 class="card-title">Data Pribadi</h5>
+            {{-- Card: Data Pribadi --}}
+            <div class="card mb-4">
+                <div class="card-header"><strong>Data Pribadi</strong></div>
+                <div class="card-body">
                     <div class="row">
                         @foreach ([['nama', 'Nama'], ['nip_nrp_nipppk_nipb', 'NIP/NRP/NIPPPK/NIPB'], ['tempat_lahir', 'Tempat Lahir'], ['tgl_lahir', 'Tanggal Lahir', 'date'], ['umur_tahun', 'Umur (Tahun)', 'number'], ['umur_bulan', 'Umur (Bulan)', 'number'], ['nik', 'NIK']] as $input)
                             <div
@@ -50,9 +50,13 @@
                             </select>
                         </div>
                     </div>
+                </div>
+            </div>
 
-                    {{-- Data Kepegawaian --}}
-                    <h5 class="card-title">Data Kepegawaian</h5>
+            {{-- Card: Data Kepegawaian --}}
+            <div class="card mb-4">
+                <div class="card-header"><strong>Data Kepegawaian</strong></div>
+                <div class="card-body">
                     <div class="row">
                         @foreach ([['status_kepegawaian', 'Status Kepegawaian'], ['jabatan_terakhir', 'Jabatan Terakhir'], ['tmt_jabatan', 'TMT Jabatan', 'date'], ['tmt_kerja_di_rsud', 'TMT Kerja di RSUD', 'date'], ['lama_kerja_tahun', 'Lama Kerja (Tahun)', 'number'], ['lama_kerja_bulan', 'Lama Kerja (Bulan)', 'number'], ['gol', 'Golongan'], ['pangkat_gol', 'Pangkat Golongan'], ['tmt_gol', 'TMT Golongan', 'date'], ['no_sk', 'No SK'], ['tgl_sk', 'Tanggal SK', 'date']] as $input)
                             <div
@@ -63,9 +67,13 @@
                             </div>
                         @endforeach
                     </div>
+                </div>
+            </div>
 
-                    {{-- Data Pendidikan --}}
-                    <h5 class="card-title">Data Pendidikan</h5>
+            {{-- Card: Data Pendidikan --}}
+            <div class="card mb-4">
+                <div class="card-header"><strong>Data Pendidikan</strong></div>
+                <div class="card-body">
                     <div class="row">
                         @foreach ([['jenjang_pendidikan', 'Jenjang Pendidikan'], ['pendidikan_terakhir', 'Pendidikan Terakhir']] as $input)
                             <div class="mb-3 col-md-6">
@@ -75,9 +83,13 @@
                             </div>
                         @endforeach
                     </div>
+                </div>
+            </div>
 
-                    {{-- Data Alamat --}}
-                    <h5 class="card-title">Data Alamat</h5>
+            {{-- Card: Data Alamat --}}
+            <div class="card mb-4">
+                <div class="card-header"><strong>Data Alamat</strong></div>
+                <div class="card-body">
                     <div class="row">
                         @foreach ([['alamat_ktp', 'Alamat KTP', 6], ['desa', 'Desa', 3], ['kecamatan', 'Kecamatan', 3], ['kabupaten', 'Kabupaten', 3], ['agama', 'Agama', 3]] as [$name, $label, $col])
                             <div class="mb-3 col-md-{{ $col }}">
@@ -87,9 +99,13 @@
                             </div>
                         @endforeach
                     </div>
+                </div>
+            </div>
 
-                    {{-- Lainnya --}}
-                    <h5 class="card-title">Lainnya</h5>
+            {{-- Card: Lainnya --}}
+            <div class="card mb-4">
+                <div class="card-header"><strong>Lainnya</strong></div>
+                <div class="card-body">
                     <div class="row">
                         @foreach ([['npwp', 'NPWP'], ['ruangan', 'Ruangan'], ['status', 'Status'], ['status_nakes', 'Status Nakes'], ['keterangan', 'Keterangan', 12]] as $input)
                             <div class="mb-3 col-md-{{ $input[2] ?? 6 }}">
@@ -99,10 +115,12 @@
                             </div>
                         @endforeach
                     </div>
-
-                    <button type="submit" class="btn btn-primary">Update Data</button>
-                </form>
+                </div>
             </div>
-        </div>
+
+            <div class="text-end">
+                <button type="submit" class="btn btn-primary">Update Data</button>
+            </div>
+        </form>
     </div>
 @endsection
