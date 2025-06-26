e49200101d373190182afcf4467376f9d36476a2
Sigit Putra Prabowo <11230220@nusamandiri.ac.id>
2025-06-26 13:47:59 +0700

    update

--- resources/views/pages/karyawan/edit.blade.php
+++ resources/views/pages/karyawan/edit.blade.php
@@ -30,67 +30,74 @@
                     @csrf
                     @method('PUT')
 
+                    {{-- Data Pribadi --}}
                     <h5 class="card-title">Data Pribadi</h5>
                     <div class="row">
-                        <x-form.input col="6" label="Nama" name="nama" :value="$karyawan->nama" required />
-                        <x-form.input col="6" label="NIP/NRP/NIPPPK/NIPB" name="nip_nrp_nipppk_nipb"
-                            :value="$karyawan->nip_nrp_nipppk_nipb" required />
-                        <x-form.input col="6" label="Tempat Lahir" name="tempat_lahir" :value="$karyawan->tempat_lahir" required />
-                        <x-form.input type="date" col="6" label="Tanggal Lahir" name="tgl_lahir" :value="$karyawan->tgl_lahir"
-                            required />
-                        <x-form.input col="3" label="Umur (Tahun)" name="umur_tahun" :value="$karyawan->umur_tahun" required />
-                        <x-form.input col="3" label="Umur (Bulan)" name="umur_bulan" :value="$karyawan->umur_bulan" required />
-                        <x-form.select col="3" label="Jenis Kelamin" name="jk" :value="$karyawan->jk"
-                            :options="['L' => 'Laki-laki', 'P' => 'Perempuan']" />
-                        <x-form.input col="3" label="NIK" name="nik" :value="$karyawan->nik" required />
+                        @foreach ([['nama', 'Nama'], ['nip_nrp_nipppk_nipb', 'NIP/NRP/NIPPPK/NIPB'], ['tempat_lahir', 'Tempat Lahir'], ['tgl_lahir', 'Tanggal Lahir', 'date'], ['umur_tahun', 'Umur (Tahun)', 'number'], ['umur_bulan', 'Umur (Bulan)', 'number'], ['nik', 'NIK']] as $input)
+                            <div
+                                class="mb-3 col-md-{{ in_array($input[0], ['umur_tahun', 'umur_bulan', 'nik']) ? '3' : '6' }}">
+                                <label for="{{ $input[0] }}" class="form-label">{{ $input[1] }}</label>
+                                <input type="{{ $input[2] ?? 'text' }}" class="form-control" id="{{ $input[0] }}"
+                                    name="{{ $input[0] }}" value="{{ $karyawan[$input[0]] ?? '' }}">
+                            </div>
+                        @endforeach
+
+                        <div class="mb-3 col-md-3">
+                            <label for="jk" class="form-label">Jenis Kelamin</label>
+                            <select class="form-select" id="jk" name="jk">
+                                <option value="L" {{ $karyawan->jk == 'L' ? 'selected' : '' }}>Laki-laki</option>
+                                <option value="P" {{ $karyawan->jk == 'P' ? 'selected' : '' }}>Perempuan</option>
+                            </select>
+                        </div>
                     </div>
 
+                    {{-- Data Kepegawaian --}}
                     <h5 class="card-title">Data Kepegawaian</h5>
                     <div class="row">
-                        <x-form.input col="6" label="Status Kepegawaian" name="status_kepegawaian" :value="$karyawan->status_kepegawaian"
-                            required />
-                        <x-form.input col="6" label="Jabatan Terakhir" name="jabatan_terakhir" :value="$karyawan->jabatan_terakhir"
-                            required />
-                        <x-form.input type="date" col="4" label="TMT Jabatan" name="tmt_jabatan"
-                            :value="$karyawan->tmt_jabatan" />
-                        <x-form.input type="date" col="4" label="TMT Kerja di RSUD" name="tmt_kerja_di_rsud"
-                            :value="$karyawan->tmt_kerja_di_rsud" />
-                        <x-form.input col="2" label="Lama Kerja (Tahun)" name="lama_kerja_tahun"
-                            :value="$karyawan->lama_kerja_tahun" />
-                        <x-form.input col="2" label="Lama Kerja (Bulan)" name="lama_kerja_bulan"
-                            :value="$karyawan->lama_kerja_bulan" />
-                        <x-form.input col="3" label="Golongan" name="gol" :value="$karyawan->gol" />
-                        <x-form.input col="3" label="Pangkat Golongan" name="pangkat_gol" :value="$karyawan->pangkat_gol" />
-                        <x-form.input type="date" col="3" label="TMT Golongan" name="tmt_gol"
-                            :value="$karyawan->tmt_gol" />
-                        <x-form.input col="3" label="No SK" name="no_sk" :value="$karyawan->no_sk" />
-                        <x-form.input type="date" col="3" label="Tanggal SK" name="tgl_sk" :value="$karyawan->tgl_sk" />
+                        @foreach ([['status_kepegawaian', 'Status Kepegawaian'], ['jabatan_terakhir', 'Jabatan Terakhir'], ['tmt_jabatan', 'TMT Jabatan', 'date'], ['tmt_kerja_di_rsud', 'TMT Kerja di RSUD', 'date'], ['lama_kerja_tahun', 'Lama Kerja (Tahun)', 'number'], ['lama_kerja_bulan', 'Lama Kerja (Bulan)', 'number'], ['gol', 'Golongan'], ['pangkat_gol', 'Pangkat Golongan'], ['tmt_gol', 'TMT Golongan', 'date'], ['no_sk', 'No SK'], ['tgl_sk', 'Tanggal SK', 'date']] as $input)
+                            <div
+                                class="mb-3 col-md-{{ in_array($input[0], ['lama_kerja_tahun', 'lama_kerja_bulan', 'gol', 'pangkat_gol']) ? '3' : '6' }}">
+                                <label for="{{ $input[0] }}" class="form-label">{{ $input[1] }}</label>
+                                <input type="{{ $input[2] ?? 'text' }}" class="form-control" id="{{ $input[0] }}"
+                                    name="{{ $input[0] }}" value="{{ $karyawan[$input[0]] ?? '' }}">
+                            </div>
+                        @endforeach
                     </div>
 
+                    {{-- Data Pendidikan --}}
                     <h5 class="card-title">Data Pendidikan</h5>
                     <div class="row">
-                        <x-form.input col="6" label="Jenjang Pendidikan" name="jenjang_pendidikan"
-                            :value="$karyawan->jenjang_pendidikan" />
-                        <x-form.input col="6" label="Pendidikan Terakhir" name="pendidikan_terakhir"
-                            :value="$karyawan->pendidikan_terakhir" />
+                        @foreach ([['jenjang_pendidikan', 'Jenjang Pendidikan'], ['pendidikan_terakhir', 'Pendidikan Terakhir']] as $input)
+                            <div class="mb-3 col-md-6">
+                                <label for="{{ $input[0] }}" class="form-label">{{ $input[1] }}</label>
+                                <input type="text" class="form-control" id="{{ $input[0] }}"
+                                    name="{{ $input[0] }}" value="{{ $karyawan[$input[0]] ?? '' }}">
+                            </div>
+                        @endforeach
                     </div>
 
+                    {{-- Data Alamat --}}
                     <h5 class="card-title">Data Alamat</h5>
                     <div class="row">
-                        <x-form.input col="6" label="Alamat KTP" name="alamat_ktp" :value="$karyawan->alamat_ktp" />
-                        <x-form.input col="3" label="Desa" name="desa" :value="$karyawan->desa" />
-                        <x-form.input col="3" label="Kecamatan" name="kecamatan" :value="$karyawan->kecamatan" />
-                        <x-form.input col="3" label="Kabupaten" name="kabupaten" :value="$karyawan->kabupaten" />
-                        <x-form.input col="3" label="Agama" name="agama" :value="$karyawan->agama" />
+                        @foreach ([['alamat_ktp', 'Alamat KTP', 6], ['desa', 'Desa', 3], ['kecamatan', 'Kecamatan', 3], ['kabupaten', 'Kabupaten', 3], ['agama', 'Agama', 3]] as [$name, $label, $col])
+                            <div class="mb-3 col-md-{{ $col }}">
+                                <label for="{{ $name }}" class="form-label">{{ $label }}</label>
+                                <input type="text" class="form-control" id="{{ $name }}"
+                                    name="{{ $name }}" value="{{ $karyawan[$name] ?? '' }}">
+                            </div>
+                        @endforeach
                     </div>
 
+                    {{-- Lainnya --}}
                     <h5 class="card-title">Lainnya</h5>
                     <div class="row">
-                        <x-form.input col="6" label="NPWP" name="npwp" :value="$karyawan->npwp" />
-                        <x-form.input col="6" label="Ruangan" name="ruangan" :value="$karyawan->ruangan" />
-                        <x-form.input col="6" label="Status" name="status" :value="$karyawan->status" />
-                        <x-form.input col="6" label="Status Nakes" name="status_nakes" :value="$karyawan->status_nakes" />
-                        <x-form.input col="12" label="Keterangan" name="keterangan" :value="$karyawan->keterangan" />
+                        @foreach ([['npwp', 'NPWP'], ['ruangan', 'Ruangan'], ['status', 'Status'], ['status_nakes', 'Status Nakes'], ['keterangan', 'Keterangan', 12]] as $input)
+                            <div class="mb-3 col-md-{{ $input[2] ?? 6 }}">
+                                <label for="{{ $input[0] }}" class="form-label">{{ $input[1] }}</label>
+                                <input type="text" class="form-control" id="{{ $input[0] }}"
+                                    name="{{ $input[0] }}" value="{{ $karyawan[$input[0]] ?? '' }}">
+                            </div>
+                        @endforeach
                     </div>
 
                     <button type="submit" class="btn btn-primary">Update Data</button>
