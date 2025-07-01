83e379fc8dd3a36a85c58fe172c9d6618c22fb0f
Sigit Putra Prabowo <11230220@nusamandiri.ac.id>
2025-07-01 10:55:23 +0700

    sss

--- resources/views/pages/karyawan/create.blade.php
+++ resources/views/pages/karyawan/create.blade.php
@@ -16,6 +16,17 @@
             </div>
         </div>
 
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul class="mb-0">
+                    @foreach ($errors->all() as $err)
+                        <li>{{ $err }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+
         @if (session('success'))
             <div class="alert alert-success">{{ session('success') }}</div>
         @endif
