c16148517b7eafb76a4158dabcc65b9010af7b2d
Sigit Putra Prabowo <11230220@nusamandiri.ac.id>
2025-07-01 10:49:46 +0700

    update

--- app/Models/Karyawan.php
+++ app/Models/Karyawan.php
@@ -11,7 +11,11 @@ class Karyawan extends Model
 
     protected $table = 'karyawans';
 
-    protected $fillable = ['nama', 'nip_nrp_nipppk_nipb', 'status_kepegawaian', 'tempat_lahir', 'tgl_lahir', 'umur_tahun', 'umur_bulan', 'jk', 'npwp', 'nik', 'status', 'jabatan_terakhir', 'tmt_jabatan', 'tmt_kerja_di_rsud', 'lama_kerja_tahun', 'lama_kerja_bulan', 'gol', 'tmt_gol', 'no_sk', 'tgl_sk', 'keterangan', 'jenjang_pendidikan', 'pendidikan_terakhir', 'alamat_ktp', 'desa', 'kecamatan', 'kabupaten', 'agama', 'ruangan', 'status_nakes'];
+    // Hanya isi kolom yang bisa diisi user (exclude: id, created_at, updated_at)
+    protected $fillable = ['nama', 'nip_nrp_nipppk_nipb', 'status_kepegawaian', 'tempat_lahir', 'tgl_lahir', 'umur_tahun', 'umur_bulan', 'jk', 'npwp', 'nik', 'status', 'jabatan_terakhir', 'tmt_jabatan', 'tmt_kerja_di_rsud', 'lama_kerja_tahun', 'lama_kerja_bulan', 'gol', 'pangkat_gol', 'tmt_gol', 'no_sk', 'tgl_sk', 'keterangan', 'jenjang_pendidikan', 'pendidikan_terakhir', 'alamat_ktp', 'desa', 'kecamatan', 'kabupaten', 'agama', 'ruangan', 'status_nakes'];
+
+    // Laravel otomatis kelola created_at dan updated_at
+    public $timestamps = true;
 
     public function str()
     {
