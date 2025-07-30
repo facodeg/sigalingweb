12c4196367fb15072f3a170614859b88b8513242
Sigit Putra Prabowo <11230220@nusamandiri.ac.id>
2025-07-30 09:15:21 +0700

    update

--- resources/views/pages/peranggota/rincian.blade.php
+++ resources/views/pages/peranggota/rincian.blade.php
@@ -322,7 +322,7 @@
                             </li> --}}
                             <li class="nav-item" role="presentation">
                                 <a class="nav-link active" id="pinjaman-tab" data-bs-toggle="tab" href="#pinjaman"
-                                    role="tab" aria-selected="false">
+                                    role="tab" aria-selected="true">
                                     <div class="d-flex align-items-center">
                                         <div class="tab-icon"><i class='bx bx-credit-card font-18 me-1'></i></div>
                                         <div class="tab-title">Data Pendidikan</div>
